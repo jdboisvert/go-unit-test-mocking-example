e44e74b7c6b7c67e4619e133de0a0e23430f5eaf
jdboisvert <info.jeffreyboisvert@gmail.com>
2022-09-30 16:19:05 -0400

    Moved comment down a line just for readability in GitHub.

--- main.go
+++ main.go
@@ -10,7 +10,8 @@ func main() {
 	// First create the main struct to be used which is the implementation of the Interface
 	repository := user_service.UserRepository{}
 
-	// Then create the service to be used which uses the interface (duck typing so since the above implements all functions of interface it IS A UserRepository)
+	// Then create the service to be used which uses the interface
+	// (duck typing so since the above implements all functions of interface it IS A UserRepository)
 	service := user_service.UserService{UserRepositoryInterface: repository}
 
 	// Should print out users correctly.
